df9309e1b8967a720283d3e1c642450ee3ea265e
Jessica Wolff <jleahwolff@gmail.com>
2020-03-19 00:24:07 -0500

    Saved React App

---
+++ cod/src/App.js
@@ -0,0 +1,13 @@
+import React, { useState }from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>COD API Player Statistics</h1>
+      <h2>Here's the API website</h2>
+    </div>
+  );
+}
+
+export default App;
